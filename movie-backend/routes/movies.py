a3623cb34d84e6807a1304ea3f7a108961e7250a
MITHULVS <vsmithul2007@gmail.com>
2026-06-22 04:08:13 +0000

    feat: Add routes for genres,movies,admin,and auth

---
+++ movie-backend/routes/movies.py
@@ -0,0 +1,50 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from database.database import get_db
+from services.movie_service import (
+    get_movies_service,
+    get_movie_service,
+    search_movies_service,
+    filter_movies_service
+)
+
+router = APIRouter(prefix="/movies", tags=["Movies"])
+
+
+@router.get("/")
+async def get_movies(
+    db: AsyncSession = Depends(get_db)
+):
+    return await get_movies_service(db)
+
+
+@router.get("/{movie_id}")
+async def get_movie(
+    movie_id: int,
+    db: AsyncSession = Depends(get_db)
+):
+    return await get_movie_service(movie_id, db)
+
+
+@router.get("/search/")
+async def search_movies(
+    q: str,
+    db: AsyncSession = Depends(get_db)
+):
+    return await search_movies_service(q, db)
+
+
+@router.get("/filter/")
+async def filter_movies(
+    genre: str | None = None,
+    language: str | None = None,
+    year: int | None = None,
+    db: AsyncSession = Depends(get_db)
+):
+    return await filter_movies_service(
+        genre,
+        language,
+        year,
+        db
+    )
