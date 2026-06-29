5a2c74256d0a72f0ef4159ce5905e3e1ba2afa18
MITHULVS <vsmithul2007@gmail.com>
2026-06-29 07:56:48 +0000

    test: redis deployment for email1

--- movie_backend/services/auth_service.py
+++ movie_backend/services/auth_service.py
@@ -1,272 +1,144 @@
-import json
+import os
+import secrets
+import smtplib
+import logging
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from dotenv import load_dotenv
 
-from fastapi import HTTPException, status
-from sqlalchemy import select
-from sqlalchemy.ext.asyncio import AsyncSession
+load_dotenv()
 
-from movie_backend.models.user import User
-from movie_backend.schemas.auth_schema import (
-    SignupRequest,
-    LoginRequest
+logger = logging.getLogger(__name__)
+
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT"))
+EMAIL_ADDRESS = os.getenv("EMAIL_ADDRESS")
+EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
+
+BASE_DIR = os.path.dirname(
+    os.path.dirname(
+        os.path.dirname(
+            os.path.abspath(__file__)
+        )
+    )
 )
-from movie_backend.util.helpers import (
-    hash_password,
-    verify_password,
-    create_access_token
-)
-
-from movie_backend.util.emails import (
-    send_welcome_email,
-    send_otp_email,
-    generate_otp
-)
-
-from movie_backend.util.helpers import rate_limit,redis_client
-
-import uuid
-
-async def signup_service(
-    request: SignupRequest,
-    db: AsyncSession
-):
-    statement = select(User).where(
-        User.email == request.email
-    )
-
-    result = await db.execute(statement)
-
-    user = result.scalar_one_or_none()
-
-    if user:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Email already exists"
-        )
-
-    new_user = User(
-        username=request.name,
-        email=request.email,
-        hashed_password=hash_password(request.password)
-    )
-
-    db.add(new_user)
-
-    await db.commit()
-
-    await db.refresh(new_user)
-
-    # send_welcome_email(new_user.username,new_user.email)
-
-    return {
-        "name": new_user.username,
-        "email": new_user.email
-    }
 
 
-async def login_service(
-    request: LoginRequest,
-    db: AsyncSession
-):
-    statement = select(User).where(
-        User.email == request.email
-    )
+# ====================================================
+# Function 1 : Welcome Email
+# ====================================================
 
-    result = await db.execute(statement)
+def send_welcome_email(username: str, user_email: str):
+    try:
+        message = MIMEMultipart("related")
+        message["Subject"] = "Welcome to Movie App 🎬"
+        message["From"] = EMAIL_ADDRESS
+        message["To"] = user_email
 
-    user = result.scalar_one_or_none()
+        html = f"""
+        <html>
+        <body>
+            <center>
+                <img src="cid:logo" width="180">
+            </center>
 
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="Incorrect password or email or not registered"
-        )
+            <h2>Hello {username} 👋</h2>
 
-    if not verify_password(
-        request.password,
-        user.hashed_password
-    ):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect password or email"
-        )
+            <p>Welcome to Movie App.</p>
 
-    token = create_access_token(
-        {
-            "id": user.id,
-            "email": user.email
-        }
-    )
+            <p>Your account has been created successfully.</p>
 
-    return {
-        "access_token": token,
-        "token_type": "bearer"
-    }
+            <p>Enjoy watching your favourite movies 🍿</p>
 
+        </body>
+        </html>
+        """
 
-async def logout_service(
-    current_user
-):
-    return {
-        "message": "Logout successful"
-    }
+        message.attach(MIMEText(html, "html"))
 
+        image_path = os.path.join(BASE_DIR, "uploads", "logo.png")
 
-async def get_current_user_service(
-    current_user,
-    db: AsyncSession
-):
-    statement = select(User).where(
-        User.id == current_user["id"]
-    )
+        if os.path.exists(image_path):
+            with open(image_path, "rb") as img:
+                image = MIMEImage(img.read())
+                image.add_header("Content-ID", "<logo>")
+                message.attach(image)
 
-    result = await db.execute(statement)
+        with smtplib.SMTP(
+            EMAIL_HOST,
+            EMAIL_PORT,
+            timeout=15
+        ) as server:
 
-    user = result.scalar_one_or_none()
+            server.starttls()
+            server.login(
+                EMAIL_ADDRESS,
+                EMAIL_PASSWORD
+            )
+            server.send_message(message)
 
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="User not found"
-        )
+        logger.info(f"Welcome email sent to {user_email}")
 
-    return {
-        "name": user.username,
-        "email": user.email
-    }
+    except Exception as e:
+        logger.exception(f"Failed to send welcome email: {e}")
 
 
-async def get_otp_service(
-    request,
-    db: AsyncSession
-):
-    result = await db.execute(
-        select(User).where(User.email == request.email)
-    )
+# ====================================================
+# Function 2 : OTP Email
+# ====================================================
 
-    user = result.scalar_one_or_none()
+def send_otp_email(user_email: str, otp: str):
+    try:
+        message = MIMEMultipart()
+        message["Subject"] = "Movie App OTP Verification"
+        message["From"] = EMAIL_ADDRESS
+        message["To"] = user_email
 
-    if user is None:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="Email not found."
-        )
+        html = f"""
+        <html>
+        <body>
 
-    otp = generate_otp()
-    request_id = str(uuid.uuid4())
+            <h2>Email Verification</h2>
 
-    await redis_client.setex(
-        f"forgot:{request_id}",
-        300,
-        json.dumps({
-            "email": user.email,
-            "otp": otp,
-            "verified": False
-        })
-    )
+            <p>Your One Time Password (OTP) is</p>
 
-    send_otp_email(user.email, otp)
+            <h1 style="color:blue;">
+                {otp}
+            </h1>
 
-    return {
-        "message": "Check your email for the OTP.",
-        "request_id": request_id
-    }
+            <p>This OTP is valid for 5 minutes.</p>
 
+            <p>Do not share this OTP with anyone.</p>
 
-async def get_otp_verified_service(
-    request
-):
-    key = f"forgot:{request.request_id}"
+        </body>
+        </html>
+        """
 
-    data = await redis_client.get(key)
+        message.attach(MIMEText(html, "html"))
 
-    if data is None:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="OTP expired or invalid request."
-        )
+        with smtplib.SMTP(
+            EMAIL_HOST,
+            EMAIL_PORT,
+            timeout=15
+        ) as server:
 
-    data = json.loads(data)
+            server.starttls()
+            server.login(
+                EMAIL_ADDRESS,
+                EMAIL_PASSWORD
+            )
+            server.send_message(message)
 
-    if data["verified"]:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="OTP already verified."
-        )
+        logger.info(f"OTP email sent to {user_email}")
 
-    if data["otp"] != request.otp:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Invalid OTP."
-        )
-
-    data["verified"] = True
-
-    ttl = await redis_client .ttl(key)
-
-    await redis_client.setex(
-        key,
-        ttl,
-        json.dumps(data)
-    )
-
-    return {
-        "message": "OTP verified successfully.",
-        "request_id": request.request_id
-    }
+    except Exception as e:
+        logger.exception(f"Failed to send OTP email: {e}")
 
 
-async def reset_password_service(
-    request,
-    db: AsyncSession
-):
-    key = f"forgot:{request.request_id}"
+# ====================================================
+# Function 3 : Generate OTP
+# ====================================================
 
-    data = await redis_client.get(key)
-
-    if data is None:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Request expired."
-        )
-
-    data = json.loads(data)
-
-    if not data["verified"]:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="OTP not verified."
-        )
-
-    result = await db.execute(
-        select(User).where(User.email == data["email"])
-    )
-
-    user = result.scalar_one_or_none()
-
-    if user is None:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="User not found."
-        )
-
-    if verify_password(
-        request.new_password,
-        user.hashed_password
-    ):
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="New password cannot be the same as the old password."
-        )
-
-    user.hashed_password = hash_password(
-        request.new_password
-    )
-
-    await db.commit()
-    await db.refresh(user)
-
-    await redis_client .delete(key)
-
-    return {
-        "message": "Password reset successfully."
-    }
+def generate_otp():
+    return str(secrets.randbelow(900000) + 100000)
